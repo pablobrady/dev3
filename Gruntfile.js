64056115722d39cc1eafba97f8976d82b1b90198
Paul Brady <pablo.brady@gmail.com>
2015-10-10 14:21:12 -0700

    [Refactor] Added Grunt concat for .JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,11 @@ module.exports = function(grunt) {
   // Project Configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     jshint: {
-      all: ['Gruntfile.js', 'public/js/app.js']
+      all: ['Gruntfile.js', 'src/*.js']
     },
+
     sass: {
       dist: {
         files: {
@@ -13,28 +15,42 @@ module.exports = function(grunt) {
         }
       }
     },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src:  'src/<%= pkg.name %>.js',
-        dest: 'public/js/<%= pkg.name %>.min.js'
+        dest: 'src/<%= pkg.name %>.min.js'
       }
     },
+
     watch: {
       css: {
         files: '**/*.scss',
         tasks: ['sass']
       }
-    }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['src/<%= pkg.name %>.min.js'],
+        dest: 'public/js/<%= pkg.name %>_all.min.js',
+      },
+    },
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default',['jshint','uglify','watch']);
-  grunt.registerTask('dev',['jshint','uglify','sass']);
+  grunt.registerTask('default',['jshint','uglify','concat','sass']);
+  grunt.registerTask('dev',['jshint','uglify','concat','sass']);
 };
