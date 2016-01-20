7cbfd58da2aed29913d1e162494bd8a47af84a99
Paul Brady <pablo.brady@gmail.com>
2016-01-19 22:54:17 -0800

    [Refactor] Clearer formatting.  Added 'lips' branch.

--- server.js
+++ server.js
@@ -1,22 +1,67 @@
 var express = require('express');
-// var compress = require('compression'); // Enable after which from Apache
-
-// var bodyParser = require('body-parser');
 var app = express();
-var router = require('./server/router');
+/// var router = require('./server/router');
 
 //set port variable to deployed port or 8000 for local host
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8001;
 
-//set dirname to client folder to serve static assets (index.html)
+
+//set dirname to client folder to serve static ASSETS (.css, .png, .jpg)
 app.use('/', express.static(__dirname + '/public'));
-// app.use(compress());
 
-//parses all incoming data from strings to JSON
-//app.use(bodyParser.json());
 
-//Send all calls to router function
-app.use(router);
+
+/* CUSTOM HELPER METHODS - STYLIST SITE */
+
+var stylistNamespace = {};
+stylistNamespace.pageEnum = {
+  'INDEX' :        'index.html',
+  'LIPS'  :        'playground/lips/index.html'
+};
+
+var handlePageResponse = function(filename, req, res) {
+  var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  // filename == req.params.name;
+  if (!filename || filename==="") { 
+    filename = stylistNamespace.pageEnum.INDEX; 
+    console.log("ERR HANDLED:  filename = " + filename);
+  }
+  res.sendFile(filename, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', filename);
+    }
+  });
+}
+
+/* ----- */
+
+
+
+/* EXPRESS ROUTE HANDING */
+
+app.get('/', function(req, res) {
+  handlePageResponse( stylistNamespace.pageEnum.INDEX, req, res);
+});
+
+app.get('/lips', function(req, res) {
+  handlePageResponse( stylistNamespace.pageEnum.LIPS, req, res);
+});
+
+/* ----- */
+
+
 
 app.listen(port, function() {
   console.log("Listening on port " + port + "...");
